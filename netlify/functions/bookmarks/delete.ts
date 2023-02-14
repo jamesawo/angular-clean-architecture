29400cb6939202c36257e6c97aa79adc8486c73b
James Aworo <james.aworo@outlook.com>
2023-02-14 19:59:38 +0100

    added example.env file

--- netlify/functions/bookmarks/delete.ts
+++ netlify/functions/bookmarks/delete.ts
@@ -1,23 +1,23 @@
-import { MongoClient, ObjectId } from 'mongodb'
+import { MongoClient, ObjectId } from 'mongodb';
 
 const remove = async (bookmarkId: string) => {
-	if (!bookmarkId) throw new Error('Please provide a book to remove');
-	const client = new MongoClient(process.env['MONGODB_URL']);
+    if (!bookmarkId) throw new Error('Please provide a book to remove');
+    const client = new MongoClient(process.env['MONGODB_URL']);
 
-	try {
-		const database = client.db(process.env['MONGODB_NAME']);
-		const result = await database.collection("bookmarks")
-			.deleteOne({ "_id": new ObjectId(bookmarkId) });
-		return result;
+    try {
+        const database = client.db(process.env['MONGODB_NAME']);
+        const result = await database.collection("bookmarks")
+            .deleteOne({ "_id": new ObjectId(bookmarkId) });
+        return result;
 
-	} catch (error) {
+    } catch (error) {
 
-		console.log(error);
-		throw new Error(error.message);
-	}
-	finally {
-		await client.close();
-	}
+        console.log(error);
+        throw new Error(error.message);
+    }
+    finally {
+        await client.close();
+    }
 }
 
 
--- netlify/functions/bookmarks/put.ts
+++ netlify/functions/bookmarks/put.ts
@@ -1,31 +1,32 @@
 import { HandlerEvent } from "@netlify/functions";
 import { MongoClient, ObjectId } from 'mongodb'
-import { Bookmark } from './../../../src/app/lib/types.interface';
+import { BookmarkRequest } from 'src/app/data/requests/bookmark.request';
+
 
 const put = async (event: HandlerEvent) => {
-	if (!event.body) throw new Error('Please provide a bookmark to save');
+    if (!event.body) throw new Error('Please provide a bookmark to save');
 
-	const client = new MongoClient(process.env['MONGODB_URL']);
-	const bookmark: Bookmark = JSON.parse(event.body);
+    const client = new MongoClient(process.env['MONGODB_URL']);
+    const bookmark: BookmarkRequest = JSON.parse(event.body);
 
-	try {
-		const { _id, ...restBookmark } = bookmark;
-		const database = client.db(process.env['MONGODB_NAME']);
-		const result = await database.collection("bookmarks").updateOne(
-			{ _id: new ObjectId(_id) },
-			{ $set: restBookmark, },
-			{ upsert: false }
-		);
+    try {
+        const { _id, ...restBookmark } = bookmark;
+        const database = client.db(process.env['MONGODB_NAME']);
+        const result = await database.collection("bookmarks").updateOne(
+            { _id: new ObjectId(_id) },
+            { $set: restBookmark, },
+            { upsert: false }
+        );
 
-		return result;
+        return result;
 
-	} catch (error) {
-		console.log(error);
-		throw new Error(error.message);
-	}
-	finally {
-		await client.close();
-	}
+    } catch (error) {
+        console.log(error);
+        throw new Error(error.message);
+    }
+    finally {
+        await client.close();
+    }
 }
 
 export { put };
