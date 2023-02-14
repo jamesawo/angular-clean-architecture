1b2fc3bc59d747cdb576c6452b249e4756cb7f5e
James Aworo <james.aworo@outlook.com>
2023-02-14 19:59:57 +0100

    update example.env

--- netlify/functions/bookmarks/put.ts
+++ netlify/functions/bookmarks/put.ts
@@ -1,13 +1,12 @@
 import { HandlerEvent } from "@netlify/functions";
 import { MongoClient, ObjectId } from 'mongodb'
-import { BookmarkRequest } from 'src/app/data/requests/bookmark.request';
 
 
 const put = async (event: HandlerEvent) => {
     if (!event.body) throw new Error('Please provide a bookmark to save');
 
     const client = new MongoClient(process.env['MONGODB_URL']);
-    const bookmark: BookmarkRequest = JSON.parse(event.body);
+    const bookmark: any = JSON.parse(event.body);
 
     try {
         const { _id, ...restBookmark } = bookmark;
