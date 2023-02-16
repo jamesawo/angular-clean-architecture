0a24b7254a92b8f6b556495dab513c4c321520f8
James Aworo <james.aworo@outlook.com>
2023-02-16 18:01:06 +0100

    added tailwind config.js

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,15 @@
 import { Component } from '@angular/core';
+import { Subscription } from 'rxjs';
 
 @Component({
     selector: 'app-root',
     templateUrl: './app.component.html',
 })
 export class AppComponent {
-    title = 'angular-clean-architecture-serverless';
+    public title = 'angular-clean-architecture-serverless';
+    public isDarkTheme?: boolean;
+
+    private sub: Subscription = new Subscription();
+
+
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	darkMode: 'class',
-	content: [
-		"./src/**/*.{html,ts}"
-	],
-	theme: {
-		extend: {},
-	},
-	plugins: [
-		require('@tailwindcss/typography'),
+    darkMode: 'class',
+    content: [
+        "./src/**/*.{html,ts}"
+    ],
+    theme: {
+        extend: {},
+    },
+    plugins: [
+        require('@tailwindcss/typography'),
 
-	],
-	corePlugins: {
-		//preflight: false,
-	}
+    ],
+    corePlugins: {
+    }
 }
