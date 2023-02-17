dac104646ecb748a2ebb0a0ace6424dbe4c27e8c
James Aworo <james.aworo@outlook.com>
2023-02-17 12:01:45 +0100

    removed uncompleted implementation of bookmark interactor

--- src/app/data/interactors/implementations/bookmark.interactor.ts
+++ src/app/data/interactors/implementations/bookmark.interactor.ts
@@ -3,36 +3,17 @@ import { Observable, of } from 'rxjs';
 
 import { Result } from './../../../core/types/types';
 import { Param } from 'src/app/core/params/param.payload';
-import { NoParam } from 'src/app/core/params/no-param.paylod';
 import { BookmarkRequest } from '../../requests/bookmark.request';
-import { IBookmarkInteractor } from './../contracts/ibookmark.interactor';
-import { RemoveBookmarkUsecase } from './../../../domain/usecases/bookmark-usecases/remove-bookmark.usecase';
-import { UpdateBookmarkUsecase } from './../../../domain/usecases/bookmark-usecases/update-bookmark.usecase';
-import { GetOneBookmarkUsecase } from './../../../domain/usecases/bookmark-usecases/get-one-bookmark.usecase';
 import { CreateBookmarkUsecase } from './../../../domain/usecases/bookmark-usecases/create-bookmark.usecase';
-import { GetManyBookmarksUsecase } from './../../../domain/usecases/bookmark-usecases/get-many-bookmarks.usecase';
+import { IBookmarkInteractor } from './../contracts/ibookmark.interactor';
 
 
 @Injectable({ providedIn: 'root' })
 export class BookmarkInteractor implements IBookmarkInteractor {
 
     constructor(
-        private getBookmarkUsecase: GetManyBookmarksUsecase,
-        private getOneBookmarkUsecase: GetOneBookmarkUsecase,
         private createBookmarkUsecase: CreateBookmarkUsecase,
-        private updateBookmarkUsecase: UpdateBookmarkUsecase,
-        private removeBookmarkUsecase: RemoveBookmarkUsecase
     ) {
-    }
-
-    public getMany(): Observable<BookmarkRequest[]> {
-        const result = this.getBookmarkUsecase.execute(new NoParam());
-        return result;
-    }
-
-    public getOne(slug: string): Observable<BookmarkRequest> {
-        const result = this.getOneBookmarkUsecase.execute(new Param(slug));
-        return result;
     }
 
     public create(bookmark: BookmarkRequest): Observable<BookmarkRequest> {
@@ -40,14 +21,20 @@ export class BookmarkInteractor implements IBookmarkInteractor {
         return of(bookmark);
     }
 
+    public getMany(): Observable<BookmarkRequest[]> {
+        return of();
+    }
+
+    public getOne(slug: string): Observable<BookmarkRequest> {
+        return of();
+    }
+
     public update(bookmark: BookmarkRequest): Observable<Result> {
-        return this.updateBookmarkUsecase.execute(new Param(bookmark));
+        return of();
     }
 
     public delete(slug: string): Observable<Result> {
-        return this.removeBookmarkUsecase.execute(new Param(slug));
+        return of();
     }
-
-
 
 }
