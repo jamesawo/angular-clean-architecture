b02e240a520e647d2c28026e5f60a8f67ef7fb81
James Aworo <james.aworo@outlook.com>
2023-02-17 11:58:08 +0100

    added post component

---
+++ src/app/presenter/pages/blog/post/post.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-post',
+  templateUrl: './post.component.html',
+  styles: [
+  ]
+})
+export class PostComponent {
+
+}
