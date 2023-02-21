02d84296eacff87356290c6fca7101b6b1bf5ba9
James Aworo <james.aworo@outlook.com>
2023-02-21 21:57:39 +0100

    updated modal component and backdrop

--- src/app/presenter/components/shared/modal/modal.component.ts
+++ src/app/presenter/components/shared/modal/modal.component.ts
@@ -1,11 +1,35 @@
 import { Component } from '@angular/core';
+import { ModalService } from './modal.service';
 
 @Component({
-  selector: 'app-modal',
-  templateUrl: './modal.component.html',
-  styles: [
-  ]
+    selector: 'app-modal',
+    templateUrl: './modal.component.html',
+    styles: [
+        `
+			section {
+			visibility: hidden;
+			opacity: 0;
+			transition: opacity 250ms ease-in;
+		}
+		section.open {
+			visibility: inherit;
+			opacity: 1;
+		}
+		`
+    ]
 })
-export class ModalComponent {
+export class ModalComponent<T> {
+    public display = true;
+    public title = '';
 
+    public constructor(private service: ModalService<T>) {
+        this.title = service.title!;
+    }
+
+    public close = async () => {
+        this.display = false;
+        setTimeout(async () => {
+            await this.service.close()
+        }, 1000)
+    }
 }
