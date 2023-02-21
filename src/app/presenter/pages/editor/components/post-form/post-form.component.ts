80e252d16d3f2c3dc77a7c7ba1d4f90af683c1cb
James Aworo <james.aworo@outlook.com>
2023-02-21 19:32:19 +0100

    added post form component

---
+++ src/app/presenter/pages/editor/components/post-form/post-form.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-post-form',
+  templateUrl: './post-form.component.html',
+  styles: [
+  ]
+})
+export class PostFormComponent {
+
+}
