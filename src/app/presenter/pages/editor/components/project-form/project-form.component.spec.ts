8ae29ca3e34e8d6edca9a5fa68fdb45e871ef173
James Aworo <james.aworo@outlook.com>
2023-02-21 19:33:51 +0100

    added editor's project form component

---
+++ src/app/presenter/pages/editor/components/project-form/project-form.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { ProjectFormComponent } from './project-form.component';
+
+describe('ProjectFormComponent', () => {
+  let component: ProjectFormComponent;
+  let fixture: ComponentFixture<ProjectFormComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ ProjectFormComponent ]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(ProjectFormComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/presenter/pages/editor/components/project-form/project-form.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-project-form',
+  templateUrl: './project-form.component.html',
+  styles: [
+  ]
+})
+export class ProjectFormComponent {
+
+}
