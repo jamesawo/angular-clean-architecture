3c3dc98e2cb9e244d04cf0524cbb305a8df282fc
James Aworo <james.aworo@outlook.com>
2023-02-22 13:31:13 +0100

    update get project by id serverless function handler

--- netlify/functions/projects/get.ts
+++ netlify/functions/projects/get.ts
@@ -1,29 +1,41 @@
 import { HandlerEvent } from "@netlify/functions";
-import { MongoClient } from 'mongodb'
+import { MongoClient, ObjectId } from 'mongodb'
 
 const get = async (event: HandlerEvent) => {
-	if (Object.keys(event.queryStringParameters).length > 0) {
-		return await getOne(event);
-	}
-	return await getAll(event);
+    if (Object.keys(event.queryStringParameters).length > 0) {
+        return await getOne(event);
+    }
+    return await getAll(event);
 }
 
 const getAll = async (event: HandlerEvent) => {
-	const client = new MongoClient(process.env['MONGODB_URL']);
-	try {
-		await client.connect();
-		const db = client.db(process.env['MONGODB_NAME']);
-		return await db.collection('projects').find({}).sort({ created_at: -1 }).toArray();
+    const client = new MongoClient(process.env['MONGODB_URL']);
+    try {
+        await client.connect();
+        const db = client.db(process.env['MONGODB_NAME']);
+        return await db.collection('projects').find({}).sort({ created_at: -1 }).toArray();
 
-	} catch (err) {
-		throw new Error(`${err.message}`);
-	} finally {
-		await client.close();
-	}
+    } catch (err) {
+        throw new Error(`${err.message}`);
+    } finally {
+        await client.close();
+    }
 }
 
 const getOne = async (event: HandlerEvent) => {
-	return;
+    const id = event.queryStringParameters.id;
+
+    const client = new MongoClient(process.env['MONGODB_URL']);
+    try {
+        await client.connect();
+        const db = client.db(process.env['MONGODB_NAME']);
+        return await db.collection('projects').findOne({ "_id": new ObjectId(id) });
+    } catch (err) {
+        console.log(err);
+        throw new Error(`${err.message}`);
+    } finally {
+        await client.close();
+    }
 }
 
 export { get };
