def051d736d4d3d6afa3b0c24382cf142acca9b6
James Aworo <james.aworo@outlook.com>
2023-02-24 14:14:36 +0100

    test: add base test cases for modal component

--- src/app/presenter/components/shared/modal/modal.component.spec.ts
+++ src/app/presenter/components/shared/modal/modal.component.spec.ts
@@ -2,5 +2,11 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 
 describe('ModalComponent', () => {
-
+    describe('constructor', () => {
+        it('should set title property', () => { });
+    })
+    describe('close', () => {
+        it('should set display property to false', () => { });
+        it('should call service.close method', () => { });
+    })
 });
