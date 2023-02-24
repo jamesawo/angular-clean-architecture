569ca091e07960eb9c80f88d693d7d0d02d2f596
James Aworo <james.aworo@outlook.com>
2023-02-24 14:29:06 +0100

    test: add base test cases for post detail component

--- src/app/presenter/pages/post-detail/post-detail.component.spec.ts
+++ src/app/presenter/pages/post-detail/post-detail.component.spec.ts
@@ -2,4 +2,14 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 describe('PostComponent', () => {
 
+    describe('ngOnInit', () => {
+        it('should get route param from Router', () => { });
+        it('should call Router.navigateByUrl if route param is NOT present');
+        it('should call interactor.getOne with route param as input if present')
+    });
+
+    describe('setSeo', () => {
+        it('should call seoService.setSEO method', () => { });
+        it('should an object to setSEO method of seoService', () => { });
+    })
 });
