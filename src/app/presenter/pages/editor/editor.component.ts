a22791cd4fdfde23ebf626589f6400ca346bff6c
James Aworo <james.aworo@outlook.com>
2023-02-27 16:33:34 +0100

    update: editor home page title

--- src/app/presenter/pages/editor/editor.component.ts
+++ src/app/presenter/pages/editor/editor.component.ts
@@ -1,11 +1,22 @@
 import { Component } from '@angular/core';
 
+import { PageSeoService } from '../page-seo.service';
+
 @Component({
-  selector: 'app-editor',
-  templateUrl: './editor.component.html',
-  styles: [
-  ]
+    selector: 'app-editor',
+    templateUrl: './editor.component.html',
+    styles: [
+    ]
 })
 export class EditorComponent {
+
+    constructor(
+        private seoService: PageSeoService,
+    ) { }
+
+    ngOnInit(): void {
+        this.seoService.setSEO({ pageTitle: 'Editors Tab - Portfolio', });
+    }
+
 
 }
