4e2cec87bdc547a42a93a691b64730e58c0313c4
James Aworo <james.aworo@outlook.com>
2023-02-28 16:01:15 +0100

    update: remove unneccessary pipe operator in bookmark repository

--- src/app/data/datasources/remote/repo-implementations/bookmark/bookmark.repository.ts
+++ src/app/data/datasources/remote/repo-implementations/bookmark/bookmark.repository.ts
@@ -1,42 +1,40 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+
 import { map, Observable } from 'rxjs';
+
 import { Result } from 'src/app/core/types/types';
 import { BookmarkEntity } from 'src/app/domain/entities';
 import { IBookmarkRepository } from 'src/app/domain/repositories/ibookmark.repository';
 
 @Injectable({ providedIn: 'root' })
-export class BookmarkRepository implements IBookmarkRepository {
+export class BookmarkRepository extends IBookmarkRepository {
 
     public baseUrl = window.location.origin + '/.netlify/functions/bookmarks';
 
-    constructor(private http: HttpClient) { }
+    constructor(private http: HttpClient) {
+        super();
+    }
 
     public all(): Observable<BookmarkEntity[]> {
-        return this.http
-            .get<{ data: BookmarkEntity[] }>(`${this.baseUrl}`)
-            .pipe(map(x => x.data));
+        return this.http.get<BookmarkEntity[]>(`${this.baseUrl}`);
     }
 
     public createBookmark(bookmark: BookmarkEntity): Observable<Result> {
-        return this.http.post<{ data: Result }>(`${this.baseUrl}`, bookmark)
-            .pipe(map(x => x.data));
+        return this.http.post<Result>(`${this.baseUrl}`, bookmark);
     }
 
     public getBookmark(id: string): Observable<BookmarkEntity> {
         return this.http
-            .get<{ data: BookmarkEntity }>(`${this.baseUrl}?id=${id}`)
-            .pipe(map(x => x.data));
+            .get<BookmarkEntity>(`${this.baseUrl}?id=${id}`);
     }
 
     public updateBookmark(id: string, bookmark: BookmarkEntity): Observable<Result> {
-        return this.http.put<{ data: Result }>(`${this.baseUrl}?bookmarkId=${id}`, bookmark)
-            .pipe(map(x => x.data));
+        return this.http.put<Result>(`${this.baseUrl}?bookmarkId=${id}`, bookmark);
     }
 
     public removeBookmark(id: string): Observable<Result> {
-        return this.http.delete<{ data: Result }>(`${this.baseUrl}?bookmarkId=${id}`)
-            .pipe(map(x => x.data));
+        return this.http.delete<Result>(`${this.baseUrl}?bookmarkId=${id}`);
     }
 
 }
