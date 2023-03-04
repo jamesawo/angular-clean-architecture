ed0f7ba592d44634bceef4f0850596f0917ca45d
James Aworo <james.aworo@outlook.com>
2023-03-04 23:24:33 +0100

    test: update editor header component test

--- src/app/presenter/components/layouts/editor-header/editor-header.component.spec.ts
+++ src/app/presenter/components/layouts/editor-header/editor-header.component.spec.ts
@@ -1,23 +1,41 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
+
+import { LinkComponent } from './../../shared/link/link.component';
 
 import { EditorHeaderComponent } from './editor-header.component';
+fdescribe('EditorHeaderComponent', () => {
+    let fixture: ComponentFixture<EditorHeaderComponent>;
+    let component: EditorHeaderComponent;
 
-describe('EditorHeaderComponent', () => {
-  let component: EditorHeaderComponent;
-  let fixture: ComponentFixture<EditorHeaderComponent>;
+    beforeEach(async () => {
+        await TestBed.configureTestingModule({
+            declarations: [EditorHeaderComponent, LinkComponent],
+            providers: [],
+            imports: [
+                RouterModule.forRoot([])
+            ]
+        }).compileComponents();
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ EditorHeaderComponent ]
+        fixture = TestBed.createComponent(EditorHeaderComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+
+
+
+    });
+
+    it('should create', () => {
+        expect(component).toBeTruthy();
     })
-    .compileComponents();
 
-    fixture = TestBed.createComponent(EditorHeaderComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+    it('should have 3 LinkComponents', () => {
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
-});
+        const { debugElement } = fixture;
+
+        const linkComponents = debugElement.queryAll(By.directive(LinkComponent));
+
+        expect(linkComponents.length).toBe(3);
+    })
+})
--- src/app/presenter/components/layouts/editor-header/editor-header.component.ts
+++ src/app/presenter/components/layouts/editor-header/editor-header.component.ts
@@ -1,10 +1,9 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-editor-header',
-  templateUrl: './editor-header.component.html',
-  styles: [
-  ]
+    selector: 'app-editor-header',
+    templateUrl: './editor-header.component.html',
+    styles: []
 })
 export class EditorHeaderComponent {
 
