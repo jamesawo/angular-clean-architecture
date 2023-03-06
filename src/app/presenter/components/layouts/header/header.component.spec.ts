fc1e666f402f83c3e4555684356e537d29017032
James Aworo <james.aworo@outlook.com>
2023-03-06 19:20:20 +0100

    test: update header component test bed

--- src/app/presenter/components/layouts/header/header.component.spec.ts
+++ src/app/presenter/components/layouts/header/header.component.spec.ts
@@ -1,23 +1,38 @@
+import { By } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { ThemeSwitcherComponent } from './../../shared/theme-switcher/theme-switcher.component';
+import { DarkThemeSwitchComponent } from './../../shared/theme-switcher/dark-theme-switch/dark-theme-switch.component';
 
 import { HeaderComponent } from './header.component';
 
 describe('HeaderComponent', () => {
-  let component: HeaderComponent;
-  let fixture: ComponentFixture<HeaderComponent>;
+    let component: HeaderComponent;
+    let fixture: ComponentFixture<HeaderComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ HeaderComponent ]
+    beforeEach(async () => {
+        await TestBed.configureTestingModule({
+            declarations: [HeaderComponent, ThemeSwitcherComponent, DarkThemeSwitchComponent],
+            providers: [],
+            imports: [RouterModule.forRoot([])]
+        })
+            .compileComponents();
+
+        fixture = TestBed.createComponent(HeaderComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    });
+
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
+
+    it('should load theme switcher component', () => {
+        const { debugElement } = fixture;
+
+        const switcherElement = debugElement.query(By.directive(ThemeSwitcherComponent));
+
+        expect(switcherElement).toBeTruthy();
     })
-    .compileComponents();
-
-    fixture = TestBed.createComponent(HeaderComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
-
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
 });
