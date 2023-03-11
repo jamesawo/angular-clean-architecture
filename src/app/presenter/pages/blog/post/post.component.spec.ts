0560ee3ec27f95707d3da7896a477144783715d8
James Aworo <james.aworo@outlook.com>
2023-03-11 21:34:50 +0100

    test:  format  PostComponent suite

--- src/app/presenter/pages/blog/post/post.component.spec.ts
+++ src/app/presenter/pages/blog/post/post.component.spec.ts
@@ -3,21 +3,21 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { PostComponent } from './post.component';
 
 describe('PostComponent', () => {
-  let component: PostComponent;
-  let fixture: ComponentFixture<PostComponent>;
+    let component: PostComponent;
+    let fixture: ComponentFixture<PostComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ PostComponent ]
-    })
-    .compileComponents();
+    beforeEach(async () => {
+        await TestBed.configureTestingModule({
+            declarations: [PostComponent]
+        })
+            .compileComponents();
 
-    fixture = TestBed.createComponent(PostComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+        fixture = TestBed.createComponent(PostComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
 });
