2fcb0bf4acec5a7acdd60e474eca64e6d4845dfb
James Aworo <james.aworo@outlook.com>
2023-03-15 15:50:46 +0100

    test: update LinkComponent test suite

--- src/app/presenter/components/shared/link/link.component.spec.ts
+++ src/app/presenter/components/shared/link/link.component.spec.ts
@@ -1,23 +1,41 @@
+import { RouterModule } from '@angular/router';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { LinkComponent } from './link.component';
 
 describe('LinkComponent', () => {
-  let component: LinkComponent;
-  let fixture: ComponentFixture<LinkComponent>;
+    let component: LinkComponent;
+    let fixture: ComponentFixture<LinkComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ LinkComponent ]
-    })
-    .compileComponents();
+    beforeEach(async () => {
+        await TestBed.configureTestingModule({
+            declarations: [LinkComponent],
+            imports: [RouterModule.forRoot([])],
+        }).compileComponents();
 
-    fixture = TestBed.createComponent(LinkComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+        fixture = TestBed.createComponent(LinkComponent);
+        component = fixture.componentInstance;
+        component.props = { link: 'fakelink.cm', title: 'FakeTitle' };
+        fixture.detectChanges();
+    });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
+
+    describe('@Input Props', () => {
+        it('should receive props as Input', () => {
+            expect(component.props).toBeDefined();
+            expect(component.props?.title).toContain('Fake');
+        });
+
+        it('should render the Input props value correctly', () => {
+            const { debugElement } = fixture;
+
+            const anchor = debugElement.nativeElement.querySelector('a');
+
+            expect(anchor.innerText).toEqual(component.props?.title);
+            expect(anchor.href).toContain(component.props?.link);
+        });
+    });
 });
