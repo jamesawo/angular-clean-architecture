191b77d7dcb9a18097990aff75db0502bf2f7692
James Aworo <james.aworo@outlook.com>
2023-03-15 14:49:34 +0100

    test: update AppComponent test to use TestBed

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,23 +1,52 @@
+import { RouterModule, ActivatedRoute } from '@angular/router';
+import { TestBed, ComponentFixture } from '@angular/core/testing';
+import { Title, Meta } from '@angular/platform-browser';
+
 import { of } from 'rxjs';
 
 import { AppComponent } from './app.component';
 
 import { PageSeoService } from './presenter/pages/page-seo.service';
+import { FooterComponent } from './presenter/components/layouts/footer/footer.component';
+import { HeaderComponent } from './presenter/components/layouts/header/header.component';
+import { ThemeSwitcherComponent } from './presenter/components/shared/theme-switcher/theme-switcher.component';
 import { ThemeSwitcherService } from './presenter/components/shared/theme-switcher/theme-switcher.service';
+import { DarkThemeSwitchComponent } from './presenter/components/shared/theme-switcher/dark-theme-switch/dark-theme-switch.component';
+import { LightThemeSwitchComponent } from './presenter/components/shared/theme-switcher/light-theme-switch/light-theme-switch.component';
 
 const MOCK_THEME_PREF = of('light');
 
-describe('AppComponent', () => {
+fdescribe('AppComponent', () => {
 
     let component: AppComponent;
     let themeService: jasmine.SpyObj<ThemeSwitcherService>;
     let seoService: jasmine.SpyObj<PageSeoService>;
+    let fixture: ComponentFixture<AppComponent>;
 
 
     beforeEach(async () => {
         themeService = jasmine.createSpyObj('ThemeSwitcherService', [], { pref$: MOCK_THEME_PREF });
         seoService = jasmine.createSpyObj('PageSeoService', ['setSEO']);
-        component = new AppComponent(themeService, seoService);
+
+        TestBed.configureTestingModule({
+            declarations: [
+                HeaderComponent,
+                FooterComponent,
+                ThemeSwitcherComponent,
+                DarkThemeSwitchComponent,
+                LightThemeSwitchComponent
+            ],
+            providers: [
+                { provide: ThemeSwitcherService, useValue: themeService },
+                { provide: PageSeoService, useValue: seoService },
+            ],
+            imports: [RouterModule.forRoot([])]
+        }).compileComponents()
+
+        fixture = TestBed.createComponent(AppComponent)
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+
     });
 
     it('should create the app component', () => {
@@ -48,6 +77,17 @@ describe('AppComponent', () => {
             component.ngOnInit();
 
             expect(seoService.setSEO).toHaveBeenCalled();
+        });
+
+        it('should set page title', () => {
+            let title = TestBed.inject(Title);
+            let meta = TestBed.inject(Meta);
+
+            component['seoService'] = new PageSeoService(meta, title);
+            component.ngOnInit();
+
+            expect(seoService.setSEO).toHaveBeenCalled();
+            expect(title.getTitle()).toContain("Welcome to my Personal");
         });
     });
 
