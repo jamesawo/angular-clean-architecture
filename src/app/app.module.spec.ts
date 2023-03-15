4ced8b66d68ec5d6ee6d5aaa38e86c7bada936f8
James Aworo <james.aworo@outlook.com>
2023-03-15 19:53:31 +0100

    test: add smoke test for AppModule

--- src/app/app.module.spec.ts
+++ src/app/app.module.spec.ts
@@ -3,14 +3,15 @@ import { TestBed } from '@angular/core/testing';
 import { AppModule } from './app.module';
 
 describe('AppModule', () => {
-    TestBed.configureTestingModule({
-        imports: [AppModule]
-    })
+    beforeEach(() => {
+        TestBed.configureTestingModule({
+            imports: [AppModule],
+        });
+    });
 
     it('should initialize', () => {
-        debugger;
         const module = TestBed.inject(AppModule);
 
         expect(module).toBeTruthy();
-    })
+    });
 });
