84954d7fdbba80d893055c49e0e18ca830685d7e
James Aworo <james.aworo@outlook.com>
2023-03-18 21:05:26 +0100

    update: changed 404 component page

--- src/app/presenter/components/shared/not-found/not-found.component.spec.ts
+++ src/app/presenter/components/shared/not-found/not-found.component.spec.ts
@@ -2,22 +2,21 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { NotFoundComponent } from './not-found.component';
 
-describe('NotFoundComponent', () => {
-  let component: NotFoundComponent;
-  let fixture: ComponentFixture<NotFoundComponent>;
+fdescribe('NotFoundComponent', () => {
+    let component: NotFoundComponent;
+    let fixture: ComponentFixture<NotFoundComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ NotFoundComponent ]
-    })
-    .compileComponents();
+    beforeEach(async () => {
+        await TestBed.configureTestingModule({
+            declarations: [NotFoundComponent],
+        }).compileComponents();
 
-    fixture = TestBed.createComponent(NotFoundComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+        fixture = TestBed.createComponent(NotFoundComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
 });
--- src/app/presenter/components/shared/not-found/not-found.component.ts
+++ src/app/presenter/components/shared/not-found/not-found.component.ts
@@ -1,11 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-not-found',
-  templateUrl: './not-found.component.html',
-  styles: [
-  ]
+    selector: 'app-not-found',
+    templateUrl: './not-found.component.html',
 })
-export class NotFoundComponent {
-
-}
+export class NotFoundComponent {}
