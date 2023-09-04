117d01169168f836dc3108e05922677a62af9193
James Aworo <james.aworo@outlook.com>
2023-09-04 23:40:02 +0100

    update(navigation): redirect blog link to sub domain: blog.jamesaworo.com

--- src/app/presenter/pages/editor/editor.functions.ts
+++ src/app/presenter/pages/editor/editor.functions.ts
@@ -1,4 +1,4 @@
-import { FormGroup } from "@angular/forms";
+import { FormGroup } from '@angular/forms';
 
 import { ModalService } from 'src/app/presenter/components/shared/modal/modal.service';
 import { ProjectRequest } from 'src/app/data/requests/project.request';
@@ -8,40 +8,42 @@ import { BookmarkRequest } from 'src/app/data/requests/bookmark.request';
 import { from, map, Observable } from 'rxjs';
 
 import { Result } from './../../../core/types/types';
-import { ToastService, ToastType } from './../../components/shared/toast/toast.service';
+import {
+    ToastService,
+    ToastType,
+} from './../../components/shared/toast/toast.service';
 
 type RequestArrUn = ProjectRequest[] | BookmarkRequest[] | PostRequest[];
 type RequestPayloadUn = ProjectRequest | BookmarkRequest | PostRequest;
 
-
 const isInvalidControl = (controlName: string, form: FormGroup): boolean => {
     const control = form?.controls[controlName];
     return control?.touched == true && control?.status === 'INVALID';
-}
+};
 
 const parseDate = (value?: string) => {
     if (!value) return new Date().toISOString().slice(0, 10);
 
     const parse = Date.parse(value);
     return new Date(parse).toISOString().slice(0, 10);
-}
+};
 
 const updateTags = (form: FormGroup) => {
     const { tags } = form.value;
-    return tags?.split(',') ?? []
-}
+    return tags?.split(',') ?? [];
+};
 
 const splitText = (form: FormGroup, text: string) => {
     const textBeforeSplit = form.get(text)?.value;
     return textBeforeSplit?.split(',') ?? [];
-}
+};
 
 const getActionLink = (form: FormGroup) => {
     return {
         link: form.get('actionLink')?.value ?? '',
         title: form.get('actionTitle')?.value ?? '',
-    }
-}
+    };
+};
 
 const joinShorts = (form: FormGroup) => {
     const { short } = form.value;
@@ -49,7 +51,7 @@ const joinShorts = (form: FormGroup) => {
         return short?.split(' ').join('-');
     }
     return '';
-}
+};
 
 const isFormInvalid = (form: FormGroup) => {
     if (form.invalid) {
@@ -57,46 +59,73 @@ const isFormInvalid = (form: FormGroup) => {
         return true;
     }
     return false;
-}
+};
 
 const onHttpResponse = async (
-    response: Promise<Result>, toast: ToastService, modal?: ModalService<any, any>
+    response: Promise<Result>,
+    toast: ToastService,
+    modal?: ModalService<any, any>
 ): Promise<Result> => {
     const value = await response;
     try {
         if (value && value.acknowledged) {
-            showToast(toast, ToastType.success, 'Success', 'Action Successfully');
+            showToast(
+                toast,
+                ToastType.success,
+                'Success',
+                'Action Successfully'
+            );
             modal?.close();
         } else {
             showToast(toast, ToastType.error, 'Failed', 'Opps Action Failed');
         }
     } catch (error: any) {
         const { message } = error;
-        showToast(toast, ToastType.error, 'Failed', message ?? 'Opps Action Failed');
+        showToast(
+            toast,
+            ToastType.error,
+            'Failed',
+            message ?? 'Opps Action Failed'
+        );
     } finally {
         return value;
     }
+};
 
-}
-
-const showToast = (toast: ToastService, type: ToastType, title: string, message: string) => {
+const showToast = (
+    toast: ToastService,
+    type: ToastType,
+    title: string,
+    message: string
+) => {
     toast.show({ title, message, type });
-}
+};
 
-
-const removeItemFromListIfStatus = (status: boolean, id: string, list?: RequestArrUn): void => {
+const removeItemFromListIfStatus = (
+    status: boolean,
+    id: string,
+    list?: RequestArrUn
+): void => {
     if (status && list && list.length) {
-        const index = list.findIndex(bookmark => bookmark._id === id);
+        const index = list.findIndex((bookmark) => bookmark._id === id);
         list.splice(index, 1);
     }
-}
+};
 
-const appendToObservableListIfStatus = (source: Observable<any>, item: RequestPayloadUn, result: Result): Observable<any> => {
+const appendToObservableListIfStatus = (
+    source: Observable<any>,
+    item: RequestPayloadUn,
+    result: Result
+): Observable<any> => {
     if (result.acknowledged === true) {
-        return from(source!.pipe(map((list) => [{ ...item, _id: result.insertedId }, ...list])));
+        return from(
+            source!.pipe(
+                map((list) => [{ ...item, _id: result.insertedId }, ...list])
+            )
+        );
     }
     return source;
-}
+};
 
 export {
     isInvalidControl,
@@ -109,5 +138,5 @@ export {
     splitText,
     getActionLink,
     removeItemFromListIfStatus,
-    appendToObservableListIfStatus
-}
+    appendToObservableListIfStatus,
+};
--- src/app/presenter/pages/editor/pages/editor-bookmarks/editor-bookmarks.component.ts
+++ src/app/presenter/pages/editor/pages/editor-bookmarks/editor-bookmarks.component.ts
@@ -9,86 +9,135 @@ import { ToastService } from 'src/app/presenter/components/shared/toast/toast.se
 import { ModalService } from 'src/app/presenter/components/shared/modal/modal.service';
 import { BookmarkRequest } from 'src/app/data/requests/bookmark.request';
 
-import { appendToObservableListIfStatus, isFormInvalid, joinShorts, onHttpResponse, parseDate, removeItemFromListIfStatus, updateTags } from '../../editor.functions';
-import { BookmarFormProps, BookmarkFormComponent } from './../../components/bookmark-form/bookmark-form.component';
-
+import {
+    appendToObservableListIfStatus,
+    isFormInvalid,
+    joinShorts,
+    onHttpResponse,
+    parseDate,
+    removeItemFromListIfStatus,
+    updateTags,
+} from '../../editor.functions';
+import {
+    BookmarFormProps as BookmarkFormProps,
+    BookmarkFormComponent,
+} from './../../components/bookmark-form/bookmark-form.component';
 
 @Component({
     selector: 'app-editor-bookmarks',
     templateUrl: './editor-bookmarks.component.html',
-    styles: [
-    ]
+    styles: [],
 })
 export class EditorBookmarksComponent implements OnInit {
-    public tableData: Table<BookmarkRequest> = { cols: [{ title: 'Bookmark URL' }], alias: 'url' };
+    public tableData: Table<BookmarkRequest> = {
+        cols: [{ title: 'Bookmark URL' }],
+        alias: 'url',
+    };
     public form: FormGroup = new FormGroup({});
     public isLoading = false;
 
     public constructor(
         private bookmarkInteractor: BookmarkInteractor,
-        private modalService: ModalService<BookmarkFormComponent, BookmarFormProps>,
+        private modalService: ModalService<
+            BookmarkFormComponent,
+            BookmarkFormProps
+        >,
         private toastService: ToastService
-    ) { }
+    ) {}
 
     ngOnInit(): void {
         this.setTableData();
     }
 
     public setTableData = () => {
-        this.tableData.action = { onEdit: this.onEditPost, onRemove: this.onRemoveBookmark }
-        this.tableData.data$ = this.bookmarkInteractor.getMany().pipe(shareReplay());
-    }
+        this.tableData.action = {
+            onEdit: this.onEditPost,
+            onRemove: this.onRemoveBookmark,
+        };
+        this.tableData.data$ = this.bookmarkInteractor
+            .getMany()
+            .pipe(shareReplay());
+    };
 
     public openModal = async (): Promise<void> => {
         await this.modalService.open(
             { component: BookmarkFormComponent, modalTitle: 'Create Bookmark' },
-            { inputTitle: 'defaultValue', inputValue: this.getModalProps(new BookmarkRequest()) }
+            {
+                inputTitle: 'defaultValue',
+                inputValue: this.getModalProps(new BookmarkRequest()),
+            }
         );
-    }
+    };
 
-    private onEditPost = async (id: string, post?: BookmarkRequest): Promise<void> => {
+    private onEditPost = async (
+        id: string,
+        post?: BookmarkRequest
+    ): Promise<void> => {
         await this.modalService.open(
             { component: BookmarkFormComponent, modalTitle: 'Update Bookmark' },
-            { inputTitle: 'defaultValue', inputValue: this.getModalProps(post!) }
+            {
+                inputTitle: 'defaultValue',
+                inputValue: this.getModalProps(post!),
+            }
         );
-    }
+    };
 
-    private onRemoveBookmark = async (id: string, bookmarks?: BookmarkRequest[]): Promise<void> => {
+    private onRemoveBookmark = async (
+        id: string,
+        bookmarks?: BookmarkRequest[]
+    ): Promise<void> => {
         const result: boolean = confirm('Are you sure?');
         if (id && result) {
             const response = firstValueFrom(this.bookmarkInteractor.delete(id));
             const status = await onHttpResponse(response, this.toastService);
             removeItemFromListIfStatus(status.acknowledged, id, bookmarks);
         }
-    }
+    };
 
     private onSaveBookmark = async () => {
         if (isFormInvalid(this.form)) return;
 
         this.isLoading = true;
         const bookmark: BookmarkRequest = {
-            ...this.form.value, tags: updateTags(this.form),
-            short: joinShorts(this.form)
+            ...this.form.value,
+            tags: updateTags(this.form),
+            short: joinShorts(this.form),
         };
         const response = firstValueFrom(this.bookmarkInteractor.save(bookmark));
-        const result = await onHttpResponse(response, this.toastService, this.modalService);
-        this.tableData.data$ = appendToObservableListIfStatus(this.tableData.data$!, bookmark, result);
-    }
+        const result = await onHttpResponse(
+            response,
+            this.toastService,
+            this.modalService
+        );
+        this.tableData.data$ = appendToObservableListIfStatus(
+            this.tableData.data$!,
+            bookmark,
+            result
+        );
+    };
 
     private setForm = (bookmark?: BookmarkRequest) => {
         this.isLoading = false;
         this.form = new FormGroup({
             _id: new FormControl(bookmark?._id),
-            tags: new FormControl(bookmark?.tags?.toString(), [Validators.required]),
+            tags: new FormControl(bookmark?.tags?.toString(), [
+                Validators.required,
+            ]),
             short: new FormControl(bookmark?.short, [Validators.required]),
             url: new FormControl(bookmark?.url, [Validators.required]),
-            date: new FormControl(parseDate(bookmark?.date), [Validators.required]),
-        })
-    }
+            date: new FormControl(parseDate(bookmark?.date), [
+                Validators.required,
+            ]),
+        });
+    };
 
-    private getModalProps = (post: BookmarkRequest): BookmarFormProps => {
+    private getModalProps = (post: BookmarkRequest): BookmarkFormProps => {
         this.setForm(post);
-        return { form: this.form, action: this.onSaveBookmark, data: post, isLoading: this.isLoading };
-    }
-
+        return {
+            form: this.form,
+            action: this.onSaveBookmark,
+            data: post,
+            isLoading: this.isLoading,
+        };
+    };
 }
